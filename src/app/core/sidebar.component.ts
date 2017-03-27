0919d0a7deed74b400efc715d1b25f2e753a899b
meloncocoo <nimda_wen@hotmail.com>
2017-03-28 02:54:28 +0800

    +initialize sidebar

--- src/app/core/sidebar.component.ts
+++ src/app/core/sidebar.component.ts
@@ -8,5 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent /*implements OnInit*/ {
   // constructor() { }
 
+  private _menus = [
+    { id: 1, title: '工作台' },
+    {
+      id: 2, title: '黑名单',
+      menus: [
+        { id: 201, title: '子菜单1' },
+        { id: 202, title: '子菜单2' },
+        { id: 203, title: '子菜单3' },
+      ]
+    }
+  ];
+
+  private _expand: boolean = true;
+
+  public get menus() {
+    return this._menus;
+  }
+
+  public get expand() {
+    return this._expand;
+  }
+
   // public ngOnInit() { }
+
+  public onExpand(): void {
+    this._expand = !this._expand;
+  }
+
+  public onSelect(item, items): void {
+    if (items) {
+      items.forEach((i) => {
+        i.active = false;
+      });
+    }
+    item.active = !item.active;
+  }
 }
